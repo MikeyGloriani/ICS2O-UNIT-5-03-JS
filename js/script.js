558372bd6119c0fa945c0288e972cf8e244900f6
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-11-22 14:48:47 -0500

    init

--- js/script.js
+++ js/script.js
@@ -8,14 +8,31 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Template-PWA/sw.js", {
-    scope: "/ICS2O-Template-PWA/",
+  navigator.serviceWorker.register("/ICS2O-UNIT-5-03-JS/sw.js", {
+    scope: "/ICS2O-UNIT-5-03-JS/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function updates the slider value.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+ function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+
+  // process
+  if (valueFromSlider >= 17) {
+    document.getElementById("answer").innerHTML =
+      "You can watch an R-rated movie" 
+  }
+
+  // process
+  else if (valueFromSlider >= 13) {
+    document.getElementById("answer").innerHTML =
+      "You can watch a PG-13 movie" 
+  }
+   // process
+   else if (valueFromSlider >= 5) {
+    document.getElementById("answer").innerHTML =
+      "You can watch a G-rated movie" 
+  }
 }
